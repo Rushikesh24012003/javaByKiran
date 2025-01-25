7ab5c398801e05a55813ca191eb6feba75a789a4
Rushikesh24012003 <83913503+Rushikesh24012003@users.noreply.github.com>
2025-01-25 18:29:44 +0530

    25/01/2025 updates

--- Kiran_Academy/src/twoDimensionalArray/A.java
+++ Kiran_Academy/src/twoDimensionalArray/A.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class A {
 	Scanner sc = new Scanner(System.in);
 	
+//	public void getProduct(int a[][] , int b[][] , int product[][]) {
+//		int s=0;
+//		for(int k=0; k<a.length ; k++) {
+//			for(int c=0 ; c<a[k].length ; c++) {
+//				product[k][c]=0;
+//				for(int z=0 ; z<a[c].length ; z++) {
+//					product[k][c]+= a[k][c]*b[k][c];
+//				}
+//			}
+//		}
+//	}
+	
 	public void getSum(int a[][] , int b[][] , int sum[][]) {
 		for(int i=0 ; i<a.length ; i++) {
 			for(int j=0 ; j<a[i].length ; j++) {
